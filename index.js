1fccce66a3817a6229906ede84fc574f2d884ea8
Manuel <git.manuel98@gmail.com>
2019-02-17 19:10:40 +0100

    Programa casi finalizado a falta de los estilos de css

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-
+let allLetra = Array();
+let mil = 0;
+let sec = 0;
+let mint = 0;
+let control;
 $(function () {
     ConnectaAjax("palabras.json");
 });
+
 
 //Funcion a la que le pasas el nombre de un archivo y atraves de
 //AJAX te lo transforma en un json con los datos que contenga.
@@ -10,7 +15,9 @@ function ConnectaAjax(archivo) {
         url: archivo,
         method: "get",
         dataType: "json",
+        //async: false,
         success: function (a) {
+
             CreaLogogrifo(a, EnumLetra(a));
         }
 
@@ -19,24 +26,31 @@ function ConnectaAjax(archivo) {
 
 //Esta funcion te crea el logogrifo visualmente.
 function CreaLogogrifo(palabras, allLetra) {
+    mil = 0;
+    sec = 0;
+    mint = 0;
+    $('#mint').text('00:')
+    $('#sec').text('00')
+    $('#mil').text('00')
+    control = setInterval('time()', 10);
     for (let filas = 0; filas < 7; filas++) {
         //Primer for para crear las filas.
         $("#all").append('<div class="row" id="row' + filas + '">');
         for (let i = 0; i < 10 - filas; i++) {
             //Segundo for para crear los cuadrados donde iran las letras
-            let valor = EscribeNum(palabras[filas].palabra[i], allLetra);
-            $("#row" + filas).append(valor+'<input onchange="EscribeAll('+valor+', this.value)" type="text" class="n'+valor+'" name="" id="" maxlength="1">');
+            let valor = EscribeNum(palabras[filas].palabra[i]);
+            $("#row" + filas).append('<p>'+valor + '</p><input onchange="EscribeAll(' + valor + ', this.value)" type="text" class="n' + valor + '" name="" id="" maxlength="1">');
         }
         //Añade la descripción
-        $("#all").append('<div class=row"> ' + palabras[filas].descripcion + ' </div> </div> ');
+        $("#all").append('<div class=row"><p> ' + palabras[filas].descripcion + ' </p></div> </div> ');
     }
+
 }
 
 
 //Enumera las letras
 function EnumLetra(palabras) {
     let letras = Array();
-    let allLetra= Array();
     //Creo el primer array para separar las palabras de una en una en letra por letra
     for (let i = 0; i < palabras.length; i++) {
         letras = (palabras[i].palabra).split("");
@@ -52,13 +66,94 @@ function EnumLetra(palabras) {
 }
 
 //Saca el valor de la letra.
-function EscribeNum(letra, allLetra){
-    return 1+($.inArray(letra, allLetra));
+function EscribeNum(letra) {
+    //console.log(allLetra)
+    return 1 + ($.inArray(letra, allLetra));
+
 }
 
-function EscribeAll(valor, texto){
-    
-    $('.n'+valor).each(function(){
+//Escribe la misma letra en los cuadrados corresponndiente.
+function EscribeAll(valor, texto) {
+
+    $('.n' + valor).each(function () {
         $(this).val(texto)
     })
+}
+
+//Comprueba palabras
+function CompruebaPalabras() {
+    let todas = true;
+    clearInterval(control)
+    for (let i = 0; i < allLetra.length; i++) {
+        if ($('.n' + (i + 1)).val() != allLetra[i]) {
+            $('.n' + (i + 1)).addClass("error")
+            todas = false;
+
+        }
+        else {
+            $('.n' + (i + 1)).addClass("correcto")
+        }
+        $('.n' + (i + 1)).prop("disabled", true)
+    }
+    $('#comprueba').css('display', 'none');
+    $('#reintentar').css('display', 'block');
+    
+    if(todas ==  true){
+        alert("!Has ganado Felicidades¡")
+    }
+    else{
+        alert("Vamos que tu puedes")
+    }
+
+}
+
+
+function JuegadeNuevo() {
+
+
+    for (let i = 0; i <= allLetra.length; i++) {
+        $('.n' + i).each(function () {
+            $(this).val("")
+            $('.n' + (i)).removeAttr("disabled");
+            $('.n' + (i)).removeClass("correcto")
+            $('.n' + (i)).removeClass("error")
+        })
+    }
+    mil = 0;
+    sec = 0;
+    mint = 0;
+    $('#mint').text('00:')
+    $('#sec').text('00')
+    $('#mil').text('00')
+    control = setInterval('time()', 10);
+   
+
+
+    $('#comprueba').css('display', 'block');
+    $('#reintentar').css('display', 'none');
+}
+
+
+// function explode(){
+//     setTimeout(explode, 15000);
+//     alert("Se acabo el tiempo!");
+//     CompruebaPalabras();
+// }
+
+
+function time() {
+    mil = mil + 1;
+    document.getElementById('mil').innerHTML = ':' + mil;
+    if (mil == 99) {
+        mil = 0;
+        sec = sec + 1;
+        document.getElementById('sec').innerHTML = sec;
+        if (sec == 60) {
+            sec = 0;
+            document.getElementById('sec').innerHTML = sec;
+            mint = mint + 1;
+            document.getElementById('mint').innerHTML = mint + ":";
+        }
+    }
+
 }
