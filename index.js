6d31c8ad72f6aba070362af2871ad6859c41c4ab
Manuel <git.manuel98@gmail.com>
2019-03-07 20:31:18 +0100

    Falta cronometro, estilos añadidos

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let allLetra = Array();
-let mil = 0;
-let sec = 0;
-let mint = 0;
+// let mil = 0;
+// let sec = 0;
+// let mint = 0;
 let control;
+let num;
 $(function () {
     ConnectaAjax("palabras.json");
 });
@@ -17,8 +18,8 @@ function ConnectaAjax(archivo) {
         dataType: "json",
         //async: false,
         success: function (a) {
-
-            CreaLogogrifo(a, EnumLetra(a));
+            let random = Math.floor(Math.random() * a.length)
+            CreaLogogrifo(a[random], EnumLetra(a[random]));
         }
 
     });
@@ -26,20 +27,14 @@ function ConnectaAjax(archivo) {
 
 //Esta funcion te crea el logogrifo visualmente.
 function CreaLogogrifo(palabras, allLetra) {
-    mil = 0;
-    sec = 0;
-    mint = 0;
-    $('#mint').text('00:')
-    $('#sec').text('00')
-    $('#mil').text('00')
-    control = setInterval('time()', 10);
+    $("#all").html("")
     for (let filas = 0; filas < 7; filas++) {
         //Primer for para crear las filas.
         $("#all").append('<div class="row" id="row' + filas + '">');
         for (let i = 0; i < 10 - filas; i++) {
             //Segundo for para crear los cuadrados donde iran las letras
             let valor = EscribeNum(palabras[filas].palabra[i]);
-            $("#row" + filas).append('<input onchange="EscribeAll(' + valor + ', this.value)" type="text" placeholder="'+valor+'" class="n' + valor + ' letras" name="" id="" maxlength="1">');
+            $("#row" + filas).append('<input onchange="EscribeAll(' + valor + ', this.value)" type="text" placeholder="' + valor + '" class="n' + valor + ' letras" value="" maxlength="1">');
         }
         //Añade la descripción
         $("#all").append('<div class=row"><p> ' + palabras[filas].descripcion + ' </p></div> </div> ');
@@ -74,10 +69,24 @@ function EscribeNum(letra) {
 
 //Escribe la misma letra en los cuadrados corresponndiente.
 function EscribeAll(valor, texto) {
-    //Recorre todo los cuadrados con la misma clase y le asigna el valor correspondiente
-    $('.n' + valor).each(function () {
-        $(this).val(texto)
-    })
+    console.log()
+    if (/[A-z]/.test(texto)) {
+        console.log(texto)
+        texto = texto.toLowerCase()
+        //Recorre todo los cuadrados con la misma clase y le asigna el valor correspondiente
+        $('.n' + valor).each(function () {
+            $(this).val(texto)
+        })
+    }
+    else {
+        $("#mError").css("display", "block");
+        num = valor;
+    }
+    if ((/[A-z]/.test($(".n" + num).val()))) {
+        console.log('dentro')
+        $("#mError").css("display", "none")
+        num = -1;
+    }
 }
 
 //Comprueba palabras
@@ -95,20 +104,19 @@ function CompruebaPalabras() {
 
         }
         else {
-            
+
             $('.n' + (i + 1)).addClass("correcto")
         }
         $('.n' + (i + 1)).prop("disabled", true)
     }
-    $('#comprueba').css('display', 'none');
-    $('#reintentar').css('display', 'inline-block');
-    
+   
+
     //Si acierta toda te lanza un mensaje como que has ganado o que has perdido en caso contrario.
-    if(todas ==  true){
-        alert("¡Has ganado Felicidades!")
+    if (todas == true) {
+        $("#puesto").text("Enhorabuena, has ganado!!")
     }
-    else{
-        alert("¡Has fallado, vuelvelo a intentar!")
+    else {
+        $("#puesto").text("Has fallado, pero no te preocupes! Puedes volver a intentarlo nuevamente!!")
     }
 
 }
@@ -116,58 +124,14 @@ function CompruebaPalabras() {
 //Reinicia el juego
 function JuegadeNuevo() {
     //Activa los input y quita los estilos.
-    for (let i = 0; i <= allLetra.length; i++) {
-        $('.n' + i).each(function () {
-            $(this).val("")
-            $('.n' + (i)).removeAttr("disabled");
-            $('.n' + (i)).removeClass("correcto")
-            $('.n' + (i)).removeClass("error")
-        })
-    }
-    //Restaura el cronometro
-    mil = 0;
-    sec = 0;
-    mint = 0;
-    $('#mint').text('00:')
-    $('#sec').text('00')
-    $('#mil').text('00')
-    control = setInterval('time()', 10);
-   
+    // for (let i = 0; i <= allLetra.length; i++) {
+    //     $('.n' + i).each(function () {
+    //         $(this).val("")
+    //         $('.n' + (i)).removeAttr("disabled");
+    //         $('.n' + (i)).removeClass("correcto")
+    //         $('.n' + (i)).removeClass("error")
+    //     })
+    // }
+    ConnectaAjax("palabras.json");
 
-    // Hace que vaya apareciendo el botón de Reintentar o de comprobar segun 
-    // haga falta.
-    $('#comprueba').css('display', 'inline-block');
-    $('#reintentar').css('display', 'none');
-}
-
-
-// function explode(){
-//     setTimeout(explode, 15000);
-//     alert("Se acabo el tiempo!");
-//     CompruebaPalabras();
-// }
-
-
-//Funcion de cronometro.
-function time() {
-     
-    mil = mil + 1;
-    if(mil == 99){
-        mil = 0;
-        sec = sec + 1;
-        if(sec == 59){
-            sec=0;
-            mint = mint + 1;
-        }
-    }
-    if(mil<10){
-        var a = document.getElementById('mil').innerHTML = ':0' + mil;
-    }
-    else var a = document.getElementById('mil').innerHTML = ':'+mil;
-    if(sec<10){
-        var b = document.getElementById('sec').innerHTML = ':0'+sec;
-    }else var b = document.getElementById('sec').innerHTML = ':' + sec;
-    if(mint<10){
-        var c = document.getElementById('mint').innerHTML = '&nbsp;0'+mint;
-    } else var c = document.getElementById('mint').innerHTML = mint;
 }
